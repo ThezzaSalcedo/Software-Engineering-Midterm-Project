220a9e939a1f9de09fa036fff750ac294512c903
Salcedo Thezza <salcedothezza@gmail.com>
2026-03-13 10:22:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-539355497-979b2",
+  "appId": "1:10993403261:web:3dc1c8dd993c1e671a7814",
+  "apiKey": "AIzaSyAjga00HlGZS4U-jg1jzV9RKRCZkKIp5cY",
+  "authDomain": "studio-539355497-979b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "10993403261"
+};
